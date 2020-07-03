9e935a8a436e9e6305730a5ead172b9acf736fed
arodrigues_freebalance <arodrigues@freebalance.com>
2020-07-03 21:46:53 +0100

    Initial commit;

---
+++ src/test/java/akr/microtraining/msscssm/MsscSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package akr.microtraining.msscssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscSsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
